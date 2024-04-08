3408080b2325823f16759050a9649e7483b981a4
ko_sakai <[ko_sakai@codenet.jp]>
2024-04-08 09:23:48 +0900

    テスト

--- web/task_vue/src/App.vue
+++ web/task_vue/src/App.vue
@@ -11,34 +11,67 @@ import ReactiveFunc from "./components/ReactiveFunc.vue";
 import OneSection from "./components/OneSection.vue";
 import WithModel from "./components/WithModel.vue";
 import OneInfo from "./components/OneInfo.vue";
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import OneMember from "./components/OneMember.vue";
 
 // const propsTitle = ref("発生した乱数");
 // const rand = Math.round(Math.random() * 100);
 // const propsContent = ref(rand);
 
-const weatherListInit = new Map<number, Weather>();
-weatherListInit.set(1, {
-  id: 1,
-  title: "今日の天気",
-  content: "今は1日中、晴れでしょう",
-});
-weatherListInit.set(2, {
-  id: 2,
-  title: "明日の天気",
-  content: "明日は1日中、雨でしょう",
-});
-weatherListInit.set(3, {
-  id: 3,
-  title: "明後日の天気",
-  content: "明後日は1日中、雪でしょう",
-});
-const weatherList = ref(weatherListInit);
+// const weatherListInit = new Map<number, Weather>();
+// weatherListInit.set(1, {
+//   id: 1,
+//   title: "今日の天気",
+//   content: "今は1日中、晴れでしょう",
+// });
+// weatherListInit.set(2, {
+//   id: 2,
+//   title: "明日の天気",
+//   content: "明日は1日中、雨でしょう",
+// });
+// weatherListInit.set(3, {
+//   id: 3,
+//   title: "明後日の天気",
+//   content: "明後日は1日中、雪でしょう",
+// });
+// const weatherList = ref(weatherListInit);
 
-interface Weather {
+// interface Weather {
+//   id: number;
+//   title: string;
+//   content: string;
+// }
+
+const memberListInit = new Map<number, Member>();
+memberListInit.set(33456, {
+  id: 33456,
+  name: "田中太郎",
+  email: "bow@example.com",
+  points: 35,
+  note: "初回入会特典あり",
+});
+memberListInit.set(47783, {
+  id: 33456,
+  name: "鈴木次郎",
+  email: "mue@example.com",
+  points: 54,
+});
+
+const memberList = ref(memberListInit);
+const totalPoints = computed((): number => {
+  let total = 0;
+  for (const member of memberList.value.values()) {
+    total += member.points;
+  }
+  return total;
+});
+
+interface Member {
   id: number;
-  title: string;
-  content: string;
+  name: string;
+  email: string;
+  points: number;
+  note?: string;
 }
 </script>
 
@@ -69,13 +102,27 @@ interface Weather {
   <!-- <Watch /> -->
   <!-- <LifeCycle /> -->
   <!-- <ReactiveFunc /> -->
-  <h1>Props 基礎</h1>
+  <!-- <h1>Props 基礎</h1>
   <section>
     <h2>ループでコンポーネントを生成</h2>
     <OneInfo v-for="[id, weather] in weatherList" 
       v-bind:key="id"
       v-bind:title="weather.title"
       v-bind:content="weather.content"
+    />
+  </section> -->
+
+  <section>
+    <h1>会員リスト</h1>
+    <p>全会員の保有ポイントの合計: {{ totalPoints }}</p>
+    <OneMember
+      v-for="[id, member] in memberList"
+      v-bind:key="id"
+      v-bind:id="id"
+      v-bind:name="member.name"
+      v-bind:email="member.email"
+      v-bind:points="member.points"
+      v-bind:note="member.note"
     />
   </section>
 </template>
