aeecee7d41f24d35908dddf05b6913320b6882b2
ko_sakai <ko_sakai@codenet.jp>
2024-04-09 11:23:39 +0900

    last vue_practice

---
+++ web/task_vue/src/components/Emit.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+interface Props {
+  rand: number;
+}
+
+// 1
+interface Emits {
+  // 2 event 又は e とすることが多い
+  (event: "createNewRand"): void;
+}
+
+defineProps<Props>();
+// 3
+const emit = defineEmits<Emits>();
+
+// 4
+const onNewRandButtonClick = (): void => {
+  // 5
+  emit("createNewRand");
+};
+</script>
+
+<template>
+  <section class="box">
+    <p>子コンポーネントで乱数を表示: {{ rand }}</p>
+    <!-- 6 -->
+    <button v-on:click="onNewRandButtonClick">新たな乱数を発生</button>
+  </section>
+</template>
+
+<style scoped>
+.box {
+  border: green 1px solid;
+  margin: 10px;
+}
+</style>
--- web/task_vue/src/components/OneMember.vue
+++ web/task_vue/src/components/OneMember.vue
@@ -1,43 +1,41 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import { computed, inject } from "vue";
+import type { Member } from "./interface";
 
 interface Props {
   id: number;
-  name: string;
-  email: string;
-  points: number;
-  note?: string;
 }
 
 const props = defineProps<Props>();
-const localPoints = ref(props.points);
+const memberList = inject("memberList") as Map<number, Member>;
+const member = computed((): Member => {
+  return memberList.get(props.id) as Member;
+});
+
 const localNote = computed((): string => {
-  let localNote = props.note;
+  let localNote = member.value.note;
   if (localNote == undefined) {
     localNote = "--";
   }
   return localNote;
 });
-
-const pointUp = (): void => {
-  localPoints.value++;
-};
 </script>
 
 <template>
   <section class="box">
-    <h4>{{ name }}さんの情報</h4>
+    <h4>{{ member.name }}さんの情報</h4>
     <dl>
       <dt>ID</dt>
       <dd>{{ id }}</dd>
       <dt>メールアドレス</dt>
-      <dd>{{ email }}</dd>
+      <dd>{{ member.email }}</dd>
       <dt>保有ポイント</dt>
-      <dd>{{ localPoints }}</dd>
+      <dd>
+        <input type="number" v-model.number="member.points" />
+      </dd>
       <dt>備考</dt>
       <dd>{{ localNote }}</dd>
     </dl>
-    <button v-on:click="pointUp">ポイント加算</button>
   </section>
 </template>
 
